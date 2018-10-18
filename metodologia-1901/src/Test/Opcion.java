4e3ec4f47760636a6e2a50597aac6a3adcd1d485
T-107 <T-107@PC199>
2018-10-18 17:40:58 -0500

    mi

---
+++ metodologia-1901/src/Test/Opcion.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+/**
+ *
+ * @author T-107
+ */
+public class Opcion {
+   String titulo;
+   boolean correcta;
+   public Opcion(String x,boolean y){
+       System.out.println("Se iniciara mi constructor");
+       titulo=x;
+       correcta=y;
+   }
+}
+
+
